8cb5a4ec3752a444bde15189347fc7d2e0edd414
gandhi_hitesh9 <gandhi.hitesh9@gmail.com>
2021-08-03 15:55:32 +0530

    Setup Apollo GraphQL Next js
    Setup Apollo GraphQL Next js

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div>
+      <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+        Back End Developer
+      </h2>
+    </div>
+  )
+}
